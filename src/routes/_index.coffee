d3fcd05ce68fbb9792904fbeefb7cdfaa52c0d0d
John-Kim <john@leftium.com>
2022-02-05 21:07:24 +0900

    Create iCalendar event.

--- src/routes/_index.coffee
+++ src/routes/_index.coffee
@@ -1,1 +1,50 @@
-hours = 20
+import '../app.css'
+
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend utc
+
+EVENT_TEMPLATE = """
+    BEGIN:VCALENDAR
+    BEGIN:VEVENT
+    DTSTART:DATETIME
+    DTSTAMP:20220205T031104Z
+    UID:FEHshing@leftium.com
+    CREATED:20210609T145454Z
+    SEQUENCE:SERIAL
+    SUMMARY:Immunity Ends (FEHshing)
+    TRANSP:TRANSPARENT
+    BEGIN:VALARM
+    ACTION:DISPLAY
+    DESCRIPTION:This is an event reminder
+    TRIGGER:-P0DT2H0M0S
+    END:VALARM
+    BEGIN:VALARM
+    ACTION:DISPLAY
+    DESCRIPTION:This is an event reminder
+    TRIGGER:-P0DT4H0M0S
+    END:VALARM
+    END:VEVENT
+    END:VCALENDAR
+    """
+
+hours = 1
+decrementHours = () ->
+    hours--
+    if hours < 0 then hours = 19
+
+incrementHours = () ->
+    hours = (hours+1) % 20
+
+nowjs = dayjs()
+
+makeEventDataUri = (hours) ->
+    nowjs = dayjs()
+
+    startDatetime = nowjs.add(19-hours, 'hour').utc().format('YYYYMMDDTHHmmss[Z]')
+    calendarEvent = EVENT_TEMPLATE.replace 'DATETIME', startDatetime
+    calendarEvent = calendarEvent.replace 'SERIAL', "#{nowjs.unix()}"
+
+    return "data:text/calendar,#{encodeURIComponent(calendarEvent)}"
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,81 @@
 
 <template lang='pug'>
     div
-        input(type=number bind:value='{hours}')
+        label Last Defense:
+        div.sublabel Without lift loss control!
+    div.input-wrapper
+        input(inputmode="decimal" 'bind:value={hours}')
+        span.units {(hours==1 ? 'hour':'hours')} ago
     div
-        span {hours}
-
+        label Now:
+        span {nowjs.format('ddd-DD h:mma')}
+    div
+        label Immunity Ends: 
+        span {nowjs.add(19-hours, 'hour').format('ddd-DD h:mma')}
+    div
+        button(on:click='{decrementHours}') -
+        button(on:click='{incrementHours}') +
+    div.button-wrapper
+        a.calendar-button(href='{makeEventDataUri(hours)}') Open iCal Event
 </template>
+
+<style>
+    div {
+        font-size: 140%;
+        margin: 24px 0;
+    }
+    label {
+        font-size: 140%;
+        font-weight: 700;
+    }
+    .sublabel {
+        font-size: 66%;
+        font-style: italic;
+        margin: 0;
+        padding-top: -10px;
+    }
+    .input-wrapper {
+        margin-top: -10px;
+    }
+    input {
+        font-size: 200%;
+        border-radius: 2px;
+
+        width: 50px;
+
+        text-align: right;
+        padding: 0 120px 0 0;
+    }
+    .units {
+        margin-left: -110px;
+        opacity: 50%;
+    }
+    .button-wrapper {
+        width: 100%;
+        margin-top: 20px;
+    }
+    button {
+        font-size: 125%;
+        width: 50%;
+        height: 60px;
+        border-radius: 1px;
+    }
+    .calendar-button {
+        display: block;
+        width: 100%;
+        padding: 20px 0px;
+        text-align: center;
+
+        background-color: darkgreen;
+        color: white;
+
+        text-decoration: none;
+        border-radius: 2px;
+    }
+
+    .calendar-button:hover {
+        background-color: green;
+    }
+
+</style>
+
